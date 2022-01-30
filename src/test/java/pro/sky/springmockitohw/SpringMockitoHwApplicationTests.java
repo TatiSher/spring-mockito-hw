12ecf1843ea5c7a93385faa5ecb8820b66c603bc
Tatiana <tanja6122212@list.ru>
2022-01-30 22:02:24 +0900

    Initial commit

---
+++ src/test/java/pro/sky/springmockitohw/SpringMockitoHwApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.springmockitohw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMockitoHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
